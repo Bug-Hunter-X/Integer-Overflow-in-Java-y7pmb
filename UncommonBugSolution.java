8afa1941a7ac6010a67843338f8aea03da1c8356
BugHunter-X <askzenai@gmail.com>
2025-01-25 00:16:07 +0100

    Add UncommonBugSolution.java file

---
+++ UncommonBugSolution.java
@@ -0,0 +1,9 @@
+public class UncommonBugSolution {
+
+    public static void main(String[] args) {
+        long x = 1000000000;
+        long y = 1000000000;
+        long z = x + y; 
+        System.out.println(z); 
+    }
+} 
